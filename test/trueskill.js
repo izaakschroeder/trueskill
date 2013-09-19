247b8de0a250f2325d0f075e6fe81fb453ec2c73
Izaak Schroeder <izaakschroeder@aelatis.com>
2013-09-19 04:12:24 -0700

    Minor updates.

--- test/trueskill.js
+++ test/trueskill.js
@@ -2,7 +2,7 @@
 var 
 	TrueSkill = require('com.izaakschroeder.trueskill'), 
 	assert = require('assert'),
-	precision = 10e-6;
+	precision = 10e-5;
 
 describe('TrueSkill', function() {
 	describe('#createRating', function() {
@@ -10,22 +10,29 @@ describe('TrueSkill', function() {
 			var ts = TrueSkill.create(), rank = ts.createRating();
 			assert.ok(.50 - ts.betterThan(rank) < precision);
 		});
+
+		it('should create mu values', function() {
+			var ts = TrueSkill.create();
+			assert.ok(.75 - ts.betterThan(ts.createRating(0.75)) < precision);
+			assert.ok(.25 - ts.betterThan(ts.createRating(0.25)) < precision);
+			assert.ok(.33 - ts.betterThan(ts.createRating(0.33)) < precision);
+		})
 	});
 
 	describe('#betterThan',function() {
 		it('should rank ratings > mu to be > 0.5', function() {
-			var ts = TrueSkill.create(), base = ts.createRating(), rank = ts.createRating(ts.mu+ts.sigma);
-			assert.ok(ts.betterThan(base) < ts.betterThan(rank));
+			var ts = TrueSkill.create();
+			assert.ok(ts.betterThan(ts.createRating()) < ts.betterThan(ts.createRating(0.6)));
 		});
 
 		it('should rank ratings < mu to be < 0.5', function() {
-			var ts = TrueSkill.create(), base = ts.createRating(), rank = ts.createRating(ts.mu-ts.sigma);
-			assert.ok(ts.betterThan(base) > ts.betterThan(rank));
+			var ts = TrueSkill.create();
+			assert.ok(ts.betterThan(ts.createRating()) > ts.betterThan(ts.createRating(0.4)));
 		});
 	});
 
 	describe('#update', function() {
-		
+
 	});
 
 	describe('#quality', function() {
